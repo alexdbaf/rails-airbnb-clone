f38b3591123ed6b526934b49882be52d81c7f4b9
alexdbaf <drappa.alessandro@gmail.com>
2017-05-31 18:49:27 +0200

    better validations

--- app/controllers/meeting_rooms_controller.rb
+++ app/controllers/meeting_rooms_controller.rb
@@ -2,31 +2,36 @@ class MeetingRoomsController < ApplicationController
 
   def index
    @meeting_rooms = MeetingRoom.all
-  end
+ end
 
-  def show
-    @meeting_room = MeetingRoom.find(params[:id])
-  end
+ def show
+  @meeting_room = MeetingRoom.find(params[:id])
+end
 
-  def new
-    @meeting_room = MeetingRoom.new
-  end
+def new
+  @meeting_room = MeetingRoom.new
+end
 
-  def create
-    @meeting_room = MeetingRoom.create(meeting_room_params)
+def create
+  @meeting_room = MeetingRoom.create(meeting_room_params)
+  if @meeting_room.save
     redirect_to meeting_room_path(@meeting_room)
+  else
+    render :new
   end
 
-  def destroy
-    @meeting_room = MeetingRoom.find(params[:id])
-    @meeting_room.destroy
-    redirect_to meeting_rooms_path
-  end
+end
 
-  private
+def destroy
+  @meeting_room = MeetingRoom.find(params[:id])
+  @meeting_room.destroy
+  redirect_to meeting_rooms_path
+end
 
-  def meeting_room_params
-    params.require(:meeting_room).permit(:id, :hourly_price, :n_people, :user_id, :morning, :afternoon, :saturday_open, :sunday_open, :location, :layout, :description, :address, :name, :photo, :photo_cache)
+private
+
+def meeting_room_params
+  params.require(:meeting_room).permit(:id, :hourly_price, :n_people, :user_id, :morning, :afternoon, :saturday_open, :sunday_open, :location, :layout, :description, :address, :name, :photo, :photo_cache)
 end
 
 end
--- app/models/meeting_room.rb
+++ app/models/meeting_room.rb
@@ -3,7 +3,9 @@ class MeetingRoom < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
   has_many :reviews, through: :bookings
-  # validates :name, :n_people, :description, presence: true
+  validates :name, :n_people, :hourly_price, :description, presence: true
+  validates :hourly_price, :n_people, numericality: true
+  validates :name, :description, :address, length: { maximum: 100 }
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
