33cccfe69631d06fbe4462db5742a56bd77f9eee
alexdbaf <drappa.alessandro@gmail.com>
2017-06-01 15:26:01 +0200

    implementing booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
 
+
   def index
     @bookings = Booking.where(user: current_user.id)
   end
@@ -9,11 +10,28 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.create(booking_params)
+    set_meeting_room
+    @booking = Booking.new(booking_params)
+    @booking.meeting_room = @meeting_room
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
 
   end
 
+  def show
+     @booking = Booking.find(params[:id])
+  end
+
   private
+
+  def set_meeting_room
+    @meeting_room = MeetingRoom.find(params[:meeting_room_id])
+  end
 
   def booking_params
     params.require(:booking).permit(:id, :user_id, :meeting_room_id, :full_price, :morning, :afternoon, :day)
