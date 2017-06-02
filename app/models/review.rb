53cc290b9da5aa0d7c9895cfebf35fdf2c8a6110
alexdbaf <drappa.alessandro@gmail.com>
2017-06-02 16:42:10 +0200

    validations modified

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: (0..5)}
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than: 6 }
 end
