2fef23c43f8ff88232e20bd3fcff7667cdb030c7
Michel Plainchamp <michel.plainchamp@gmail.com>
2017-06-02 16:34:20 +0200

    added reviews model controller and show in bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :meeting_room
   belongs_to :user
   validates :meeting_room_id, :user_id, presence: true
-  has_one :review
+  has_one :review, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
   resources :meeting_rooms do
     collection do
       get 'search'
@@ -9,5 +11,8 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:index, :edit, :show, :update, :destroy]
+  resources :bookings, only: [:index, :edit, :show, :update, :destroy] do
+    resources :reviews, only: [:index, :show, :new, :create, :destroy]
+  end
+
 end
