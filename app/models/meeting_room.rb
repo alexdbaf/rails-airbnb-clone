ab290066cd4e85c8ab858d66d302550f0925b552
alexdbaf <drappa.alessandro@gmail.com>
2017-06-02 11:25:55 +0200

    added validation photo presence true

--- app/models/meeting_room.rb
+++ app/models/meeting_room.rb
@@ -7,6 +7,7 @@ class MeetingRoom < ApplicationRecord
   validates :hourly_price, numericality: { greater_than: 0, less_than: 200 }
   validates :n_people, numericality: { only_integer: true, greater_than: 0, less_than: 100 }
   validates :name, :description, :address, length: { maximum: 100 }
+  validates :photo, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
